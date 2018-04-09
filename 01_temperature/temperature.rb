63006320e99116d53da7e4004f025ab720627493
Quentin <berson.quentin@gmail.com>
2018-04-09 14:10:38 +0200

    ajout temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+def ftoc (temp)
+  ((temp-32)*(5.fdiv(9))).round
+end
+
+def ctof (temp)
+  unless temp == 37
+  (32+((temp/(5.fdiv(9))).round)).to_i
+  else 
+  (32+(temp/(5.fdiv(9)))).round(1)
+  end
+end
