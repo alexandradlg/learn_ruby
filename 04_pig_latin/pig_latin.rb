c1d317051f5969977c6121dc90c745aba6975a90
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-09 20:18:41 +0200

    piglatinfinished

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,17 +1,34 @@
 def translate(x)
-    vowel = ["a","e","i","o","u","y", "qu"]
+    vowel = ["a","e","i","o","u","y"]
     consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"]
+    phoneme = ["qu"]
+    phonemeConsonant = ["squ"]
     translation =[]
 
     phrase = x.split(' ')
     phrase.each { |word|
 
-        if word.start_with?(*consonant)
+        if phoneme.include? word[0] + word[1]
+            l = word.length
+            word_cut = word[2...l]
+            size = word_cut.length
+            word_cut.insert(size,"#{word[0, 2]}ay")
+            translation.push(word_cut)
+        
+        elsif phonemeConsonant.include? word[0] + word[1] + word[2]
+            l = word.length
+            word_cut = word[3...l]
+            size = word_cut.length
+            word_cut.insert(size,"#{word[0, 3]}ay")
+            translation.push(word_cut)    
+
+        elsif word.start_with?(*consonant)
             l = word.length
             word_cut = word[1...l]
 
             if word_cut.start_with?(*consonant)
                 word_cut = word[2...l]
+            
                     if word_cut.start_with?(*consonant)
                         word_cut = word[3...l]
                         size = word_cut.length
@@ -33,6 +50,7 @@ def translate(x)
             l = word.length
             word.insert(l,"ay")
             translation.push(word)
+
 
         end
     }
