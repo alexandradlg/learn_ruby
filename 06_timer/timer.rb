0489fb1d3b542a3aa1356ff162c8f3f0c57a17da
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-09 21:26:20 +0200

    timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds
+  
+  def initialize(x = 0)
+      @seconds = x
+  end
+
+  def time_string
+    Time.at(seconds).utc.strftime("%H:%M:%S")
+  end
+
 end
