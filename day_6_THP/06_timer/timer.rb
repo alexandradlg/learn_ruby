5391b943f3dafa26abbe339d22de5e893ce69244
Quentin <berson.quentin@gmail.com>
2018-04-09 21:57:21 +0200

    ajout fait maison pour le 06

--- day_6_THP/06_timer/timer.rb
+++ day_6_THP/06_timer/timer.rb
@@ -5,9 +5,19 @@ class Timer
   def initialize(x = 0)
       @seconds = x
   end
-
+### Première solution, trouvée sur Internet un peu par hasard ####
   def time_string
     Time.at(seconds).utc.strftime("%H:%M:%S")
   end
 
+### Deuxième solution, fait maison ####
+  # def time_string
+  #   s = @seconds % 60
+  #   m = @seconds / 60 % 60
+  #   h = @seconds / 60 / 60
+  #   puts s
+  #   puts m 
+  #   puts h
+  #   return "#{(sprintf '%02d',h)}:#{(sprintf '%02d',m)}:#{(sprintf '%02d',s)}"
+  # end
 end
