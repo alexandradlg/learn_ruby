bf9ed137ade30cd8356507931a0ab347818adf4c
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-09 17:59:49 +0200

    vok

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -18,3 +18,19 @@ end
 #     (1..number).each { expression_array.push(expression) }
 #     expression_array.join(" ")
 # end
+
+def start_of_word(string, x)
+end
+
+def first_word(string)
+end
+
+def titleize(string)
+    array = string.split(" ")
+        array.each { |word|
+        word.capitalize.
+        }
+    word.join
+end
+
+print titleize("hello you")
