63a5c5856d2c7ed45d21081cdf71ff5fdfd36438
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-09 17:47:15 +0200

    pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+def translate(string)
+   # consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"]
+
+
+
+        if string.start_with?("b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z")
+            new_string = string[1..5]
+                if new_string.start_with?("b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z")
+                new_string = string[2..5]
+                size = new_string.length
+                new_string.insert(size, "#{string[0, 2]}ay")
+
+                elsif
+                size = new_string.length
+                new_string.insert(size, "#{string[0]}ay")
+                end
+
+        else string.start_with?("a","e","i","o","u","y") 
+            size = string.length
+            string.insert(size,"ay")
+        end
+end
+
+
+
