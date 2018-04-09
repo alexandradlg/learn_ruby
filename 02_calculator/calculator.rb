a8953a6f52141aac5243a518a92ad94537973b8a
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-09 14:13:24 +0200

    add calculator + start simon_says

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add(x,y)
+    x + y
+end
+
+def subtract(x,y)
+    x - y
+end
+
+def sum(array)
+    array.reduce(0, :+)
+end
+
+def multiply(array)
+    array.reduce(1, :*)
+end
+
+def power(x,y)
+    (x ** y)
+end
+
+def factorial(a)
+    if a == 0 || a == 1
+        1
+    else
+        a * factorial(a - 1)
+    end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,14 @@
 #write your code here
+def echo(string)
+    string
+end
+
+def shout(string)
+    string.upcase
+end
+
+def repeat(string)
+    2.times {
+    print "#{string} "
+    }
+end
