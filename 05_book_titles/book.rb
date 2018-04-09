00e9dde20b0c73340162d2bf1d10a8a996146a2f
Quentin <berson.quentin@gmail.com>
2018-04-09 21:10:27 +0200

    book.rb full

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+
+  #@book = Book.new
+attr_accessor :title
+
+  def title=(title) 
+    @title = titlize(title)
+  end
+  
+  def titlize (x)
+      sw = ["and","over","the","a","an","in","of"]
+  
+      phrase = x.split(' ')
+    phrase.each { | word | 
+      if sw.include?(word) && phrase.index(word) != 0
+        word 
+      else 
+        word.capitalize!
+      end
+      }
+    return phrase.join(' ')
+  end
 end
